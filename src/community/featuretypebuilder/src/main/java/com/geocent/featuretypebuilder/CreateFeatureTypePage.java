0cd1562e20de1db57387c1b68aac4c686bd3e13a
Stephen Mann <thanthese@gmail.com>
2012-01-10 10:30:18 -0600

    added duplicate attr names guard

--- src/community/featuretypebuilder/src/main/java/com/geocent/featuretypebuilder/CreateFeatureTypePage.java
+++ src/community/featuretypebuilder/src/main/java/com/geocent/featuretypebuilder/CreateFeatureTypePage.java
@@ -1,5 +1,6 @@
 package com.geocent.featuretypebuilder;
 
+import java.util.Collection;
 import org.geoserver.web.GeoServerSecuredPage;
 import org.apache.wicket.markup.html.basic.Label;
 import java.sql.ResultSet;
@@ -8,9 +9,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Transformer;
+import org.apache.commons.collections.list.SetUniqueList;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.HiddenField;
@@ -42,29 +48,17 @@ public class CreateFeatureTypePage extends GeoServerSecuredPage {
       "geometry",
       "point"});
 
-    ListView lv = null;
+    List<Row> defaultRows = new ArrayList(Arrays.asList(new Row[] {
+      new Row("id", "integer"),
+      new Row("version", "integer"),
+      new Row("description", "varchar(500)"),
+      new Row("the_geom", "geometry")
+    }));
+
+    ListView lv = null;  // represents attrs list in form
 
     public FeatureTypeForm(final String id) {
       super(id, new CompoundPropertyModel<ValueMap>(new ValueMap()));  // no validation
-
-      List<Row> rows = new ArrayList(Arrays.asList(new Row[] {
-        new Row("id", "integer"),
-        new Row("version", "integer"),
-        new Row("description", "varchar(500)"),
-        new Row("the_geom", "geometry")
-      }));
-
-      lv = new ListView("schema", rows) {
-        @Override
-        protected void populateItem(ListItem item) {
-          Row row = (Row) item.getModelObject();
-          item.add(new TextField("name", new PropertyModel<String>(row, "name")));
-          item.add(new DropDownChoice<String>(
-                              "type",
-                              new PropertyModel<String>(row, "type"),
-                              TYPES));
-        }
-      };
 
       add(new TextField<String>("layername").setType(String.class));
       add(new DropDownChoice(
@@ -76,32 +70,33 @@ public class CreateFeatureTypePage extends GeoServerSecuredPage {
       add(new HiddenField<String>("serialized-fields")
                 .setType(String.class)
                 .setOutputMarkupId(true));
-      add(lv);
+      add(lv = new ListView("schema", defaultRows) {
+        @Override
+        protected void populateItem(ListItem item) {
+          Row row = (Row) item.getModelObject();
+          item.add(new TextField("name", new PropertyModel<String>(row, "name")));
+          item.add(new DropDownChoice<String>(
+                          "type",
+                          new PropertyModel<String>(row, "type"),
+                          TYPES));
+        }
+      });
     }
 
     @Override
     public final void onSubmit() {
+
+      // grab values from form
       ValueMap values = getModelObject();
       String layername = DbUtils.fixName(values.getString("layername"));
       String store = values.getString("storesDropDown");
-
-      System.out.println("values = " + values.toString());
-      System.out.println("store = " + store);
-      System.out.println("lv = " + lv);
-      System.out.println("lv.getModelObject() = " + lv.getModelObject());
-
       String style = values.getString("style");
-      List<Row> newRows = parseSerialization(values.getString("serialized-fields"));
+      List<Row> rows = parseSerialization(values.getString("serialized-fields"));
 
-      // refresh attrs model
+      // refresh attrs model (if failure we'll have to refresh the form)
       List lvModel = lv.getModelObject();
       lvModel.clear();
-      for (Row row: newRows) {
-        System.out.println("row = " + row);
-        lvModel.add(row);
-      }
-      System.out.println("--- now ---");
-      System.out.println("lvModel = " + lvModel);
+      for (Row row: rows) { lvModel.add(row); }
 
       // guard: valid layer/table name
       if(layername.isEmpty()) {
@@ -109,8 +104,8 @@ public class CreateFeatureTypePage extends GeoServerSecuredPage {
         return;
       }
 
-      // guard: valid rows
-      for (Row row : newRows) {
+      // guard: all rows are valid
+      for (Row row : rows) {
         if(!row.isValid()) {
           error(String.format("Row with name '%s' and type '%s' is not valid.",
                   row.getName(),
@@ -119,14 +114,20 @@ public class CreateFeatureTypePage extends GeoServerSecuredPage {
         }
       }
 
-      // guard: table already exists
+      // guard: all row names are unique
+      if(!isAllNamesUnique(rows)) {
+        error("Duplicate attribute names are not allowed.");
+        return;
+      }
+
+      // guard: doesn't already exist
       if(DbUtils.isTableExists(layername)) {
         error(String.format("Table of name '%s' already exists", layername));
         return;
       }
 
       // create table
-      DbUtils.createTable(layername, newRows);
+      DbUtils.createTable(layername, rows);
 
       // confirm table creation
       if(DbUtils.isTableExists(layername)) {
@@ -135,6 +136,16 @@ public class CreateFeatureTypePage extends GeoServerSecuredPage {
       } else {
         error(String.format("Layer '%s' not created.", layername));
       }
+    }
+
+    private boolean isAllNamesUnique(List<Row> rows) {
+      List<Row> names = (List<Row>) CollectionUtils.collect(rows, new Transformer() {
+        @Override
+        public Object transform(Object row) {
+          return ((Row) row).getName();
+        }
+      });
+      return names.size() == new HashSet(names).size();
     }
 
     /**
@@ -146,15 +157,15 @@ public class CreateFeatureTypePage extends GeoServerSecuredPage {
       String patternStr = "([a-zA-Z0-9_ ]*)\\|([a-zA-Z0-9_() ]*)\\|?";
       Pattern pattern = Pattern.compile(patternStr);
       Matcher matcher = pattern.matcher(serialization);
-      List<Row> rws = new ArrayList<Row>();
+      List<Row> rows = new ArrayList<Row>();
       while (matcher.find()) {
         if(matcher.groupCount() >= 2) {
           String name = matcher.group(1);
           String type = matcher.group(2);
-          rws.add(new Row(name, type));
+          rows.add(new Row(name, type));
         }
       }
-      return rws;
+      return rows;
     }
 
     // Class is moved to a public class in GeoServer 2.2.
