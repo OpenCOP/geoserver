28897a0d4831ad456c19cde95d17798700ae83e7
Yancy Matherne <yancy.matherne@geocent.com>
2012-03-06 14:54:54 -0600

    opencop: Cleaned up the add edit url page.

--- src/community/opencop/src/main/java/org/geoserver/wms/web/publish/AddEditUrlPage.java
+++ src/community/opencop/src/main/java/org/geoserver/wms/web/publish/AddEditUrlPage.java
@@ -1,7 +1,6 @@
 package org.geoserver.wms.web.publish;
 
 import org.apache.wicket.Component;
-import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.Link;
@@ -12,16 +11,17 @@ import org.geoserver.catalog.FeatureTypeInfo;
 import org.geoserver.catalog.LayerInfo;
 import org.geoserver.catalog.StoreInfo;
 import org.geoserver.web.CatalogIconFactory;
-import org.geoserver.web.GeoServerApplication;
 import org.geoserver.web.GeoServerSecuredPage;
 import org.geoserver.web.wicket.GeoServerTablePanel;
 import org.geoserver.web.wicket.GeoServerDataProvider.Property;
 import com.geocent.opencop.db.util.DbUtils;
+import org.geoserver.catalog.Catalog;
 
 public class AddEditUrlPage extends GeoServerSecuredPage {
 
   AddEditUrlLayerProvider provider = new AddEditUrlLayerProvider();
   GeoServerTablePanel<LayerInfo> table;
+//    final CatalogIconFactory icons = CatalogIconFactory.get();
 
   public AddEditUrlPage() {
 
@@ -32,67 +32,60 @@ public class AddEditUrlPage extends GeoServerSecuredPage {
       protected Component getComponentForProperty(String id, IModel itemModel,
               Property<LayerInfo> property) {
         if (property == AddEditUrlLayerProvider.TYPE) {
+
+          // Get the layer type (point, line, raster, etc.) icon
           Fragment f = new Fragment(id, "iconFragment", AddEditUrlPage.this);
           f.add(new Image("layerIcon", icons.getSpecificLayerIcon((LayerInfo) itemModel.getObject())));
           return f;
-        } else if (property == AddEditUrlLayerProvider.WORKSPACE) {
-          return new Label(id, property.getModel(itemModel));
-        } else if (property == AddEditUrlLayerProvider.STORE) {
-          return new Label(id, property.getModel(itemModel));
+          
         } else if (property == AddEditUrlLayerProvider.NAME) {
           return new Label(id, property.getModel(itemModel));
+        } else if (property == AddEditUrlLayerProvider.TITLE) {
+          return new Label(id, property.getModel(itemModel));
+        } else if (property == AddEditUrlLayerProvider.EXISTS) {
+          
+          // return a check mark icon if the layer has an edit_url field
+          Fragment f = new Fragment(id, "existsFragment", AddEditUrlPage.this);
+          boolean exists = (Boolean)property.getPropertyValue((LayerInfo) itemModel.getObject());
+          if (exists) {
+            f.add(new Image("existsIcon", icons.getEnabledIcon()));
+          } else {
+            f.add(new Image("existsIcon"));
+          }
+          return f;
+          
         } else if (property == AddEditUrlLayerProvider.ADD) {
+
+          // Create the link that will do the database work to add
+          // edit_url column and rule
           Fragment f = new Fragment(id, "addEditUrlFragment", AddEditUrlPage.this);
           f.add(new Link("addEditUrlLink", itemModel) {
 
             public void onClick() {
+              // Get all the catalog objects needed
+              Catalog catalog = getCatalog();
               LayerInfo layer = (LayerInfo) getModelObject();
-
-//                      DataStore ds = null;
-                      DataStoreInfo dsInfo = null;
-//                      try {
-              // basic checks
-                        dsInfo = getCatalog().getDataStore(layer.getResource().getStore().getId());
-//                        ds = (DataStore) dsInfo.getDataStore(null);
-//                        SimpleFeatureType featureType = ds.getSchema(layer.getName());
-//                        SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
-//                        builder.init(featureType);
-//                        builder.add("edit_url", String.class);
-//
-//                        ds.updateSchema(layer.getName(), builder.buildFeatureType());
               StoreInfo store = layer.getResource().getStore();
+              DataStoreInfo dsInfo = catalog.getDataStore(store.getId());
               String layerName = layer.getName();
-              
+
+              // Do the database work
               DbUtils.addEditUrlColumn(store, layerName);
               DbUtils.setEditUrls(store, layerName);
               DbUtils.createEditUrlRule(store, layerName);
 
-              GeoServerApplication app = (GeoServerApplication) getApplication();
+              // Refresh the in memory catalog so GeoServer knows about the new field
+              FeatureTypeInfo ft = catalog.getFeatureTypeByDataStore(dsInfo, layerName);
+              catalog.getResourcePool().clear(ft);
+              catalog.getResourcePool().clear(ft.getStore());
 
-//              getCatalog().getFeatureTypeByDataStore(dsInfo, layerName);
-                
-                FeatureTypeInfo ft = getCatalog().getFeatureTypeByDataStore(dsInfo, layerName);
-                app.getCatalog().getResourcePool().clear(ft);
-                app.getCatalog().getResourcePool().clear(ft.getStore());
-              getCatalog().save(layer);
-              
               info("Added edit URLs to layer: " + layerName);
-//                      } catch (IOException ex) {
-//                        Logger.getLogger(AddEditUrlPage.class.getName()).log(Level.SEVERE, null, ex);
-//                      }
-
-              // do something here...
-              System.out.println("This button works!");
             }
           });
           return f;
         }
 
         throw new IllegalArgumentException("Don't know a property named " + property.getName());
-      }
-
-      @Override
-      protected void onSelectionUpdate(AjaxRequestTarget target) {
       }
     };
     
